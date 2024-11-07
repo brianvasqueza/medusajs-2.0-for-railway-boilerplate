8b57d96c6d3a73f47b52747874c38f665791ab3f
brianvasqueza <alexandervasquez0082@gmail.com>
2024-11-07 15:11:17 -0600

    bg

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,32 +1,41 @@
 import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
+import Image from "next/image"
+import backgroundgradient from "/../storefront/src/styles/backgroundgradient.jpg"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div className="h-[75vh] w-full border-b border-ui-border-base  relative bg-ui-bg-subtle">
+      <div className="absolute w-full h-full  z-0 overflow-hidden">
+        <Image
+          src={backgroundgradient}
+          alt="Picture of the author"
+          className=" object-cover"
+          priority
+          fill
+        ></Image>
+      </div>
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
           >
-            This is now Brians custom store! YOOOO!
+            Dress To The Latest Fashion 
           </Heading>
           <Heading
             level="h2"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Need help customizing your store?
+            Check out your next attire on our store!
           </Heading>
         </span>
-        <a
+        {/* <a
           href="https://funkyton.com/medusajs-2-0-is-finally-here/"
           target="_blank"
         >
-          <h1 style={{ textDecoration: "underline" }}>
-            Visit the tutorial
-          </h1>
-        </a>
+          <h1 style={{ textDecoration: "underline" }}>Visit the tutorial</h1>
+        </a> */}
       </div>
     </div>
   )
